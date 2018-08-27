9b99bbde6c1323c49e1bc5f30395f14eaeca9e73
Alan Dantas <alan-silva_lost@hotmail.com>
2018-08-26 23:05:09 -0300

    Item Edit and Delete

--- resources/views/admin/item/edit.blade.php
+++ resources/views/admin/item/edit.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.app')
 
-@section('title','Editar Categoria')
+@section('title','Itens')
 
 @push('css')
 
@@ -14,23 +14,57 @@
                     @include('layouts.partial.msg')
                     <div class="card">
                         <div class="card-header card-header-primary">
-                            <h4 class="card-title mt-0">EDITAR CATEGORIA</h4>
-                            <p class="card-category">Faça as devidas alteraçõs nas categorias.</p>
+                            <h4 class="card-title mt-0">ADD NOVO ITEM</h4>
+                            <p class="card-category"> Adicione um novo item.</p>
                         </div>
                         <div class="card-body">
-                            <form method="POST" action="{{route('category.update',$category->id)}}" enctype="multipart/form-data">
+                            <form method="POST" action="{{route('item.update',$item->id)}}" enctype="multipart/form-data">
                                 @csrf
                                 @method('PUT')
                                 <div class="row">
                                     <div class="col-md-12">
                                         <div class="form-group">
-                                            <label class="bmd-label-floating">Nome da Categoria</label>
-                                            <input type="text" name="name" class="form-control" value="{{$category->name}}">
+                                            <label class="bmd-label-floating">Nome do Item</label>
+                                            <input type="name" name="name" class="form-control" value="{{$item->name}}">
                                         </div>
                                     </div>
                                 </div>
-                                <br> <a href="{{route('category.index')}}" class="btn btn-danger">voltar</a>
-                                    <button type="submit" class="btn btn-primary">salvar</button>
+                                <div class="row">
+                                    <div class="col-md-6">
+                                        <div class="form-group">
+                                            <label class="bmd-label-floating">Categoria do Item</label>
+                                            <select class="form-control" name="category">
+                                                @foreach($categories as $category)
+                                                    <option {{$category->id == $item->category->id ? 'selected' : '' }} value="{{$category->id}}">{{$category->name}}</option>
+                                                @endforeach
+                                            </select>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-6">
+                                        <div class="form-group">
+                                            <label class="bmd-label-floating">Preço do Item</label>
+                                            <input type="number" class="form-control" name="price" value="{{$item->price}}">
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col-md-12">
+                                        <div class="form-group">
+                                            <label class="bmd-label-floating">Descrição do Item</label>
+                                            <textarea class="form-control" name="description" rows="3">{{$item->description}}</textarea>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col-md-12">
+                                        <label class="control-label"></label>
+                                        <img class="img-responsive thumbnail rounded" width="80" height="60" src="{{asset('uploads/item/'.$item->image)}}">
+                                        <input type="file" name="image"><br>
+                                        <small class="text-muted">JPG|PNG|JPEG.</small>
+                                    </div>
+                                </div>
+                                <a href="{{ route('item.index') }}" class="btn btn-danger">Voltar</a>
+                                <button type="submit" class="btn btn-primary">Salvar</button>
                             </form>
                         </div>
                     </div>
