4c27b40994386361a9c2bb06c4a10b11ccb13cbd
Alan Dantas <alan-silva_lost@hotmail.com>
2018-08-28 15:53:44 -0300

    Contact Message Sent

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -700,13 +700,14 @@
             <div class="container-fluid color-bg">
                 <div class="row dis-table">
                     <div class="hidden-xs col-sm-6 dis-table-cell">
-                        <h2 class="section-title">Contact With us</h2>
+                        <h2 class="section-title">Contato</h2>
                     </div>
                     <div class="col-xs-6 col-sm-6 dis-table-cell">
                         <div class="section-content">
-                            <p>16th Birn street Get Plaza (4th floar) USA</p>
-                            <p>+44 12 213584</p>
-                            <p>example@mail.com </p>
+                            <p>Av. Marechal Deodoro da Fonseca, 367</p>
+                            <p>Pitangueiras - Guarujá/SP</p>
+                            <p>13 3351-9999</p>
+                            <p>example@mail.com</p>
                         </div>
                     </div>
                 </div>
@@ -725,40 +726,32 @@
             </div>
         </section>
 
-        <div class="container-fluid">
-            <div class="row">
-                <div id="map-canvas"></div>
-            </div>
-        </div>
-
-
-
         <section class="contact-form">
             <div class="container">
                 <div class="row">
                     <div class="col-md-10 col-md-offset-1 col-sm-10 col-sm-offset-1">
                         <div class="row">
-                             <form class="contact-form" method="post" action="contact.php">
-
+                             <form class="contact-form" method="post" action="{{route('contact.send')}}">
+                                 @csrf
                                 <div class="col-md-6 col-sm-6">
                                     <div class="form-group">
-                                        <input  name="name" type="text" class="form-control" id="name" required="required" placeholder="  Name">
+                                        <input  name="name" type="text" class="form-control" id="name" placeholder="Nome">
                                     </div>
                                     <div class="form-group">
-                                        <input name="email" type="email" class="form-control" id="email" required="required" placeholder="  Email">
+                                        <input name="email" type="email" class="form-control" id="email"  placeholder="Email">
                                     </div>
                                     <div class="form-group">
-                                        <input name="subject" type="text" class="form-control" id="subject" required="required" placeholder="  Subject">
+                                        <input name="subject" type="text" class="form-control" id="subject" placeholder="Assunto">
                                     </div>
                                 </div>
 
                                 <div class="col-md-6 col-sm-6">
-                                    <textarea name="message" type="text" class="form-control" id="message" rows="7" required="required" placeholder="  Message"></textarea>
+                                    <textarea name="message" type="text" class="form-control" id="message" rows="7" placeholder="Mensagem"></textarea>
                                 </div>
 
                                 <div class="col-md-6 col-md-offset-3 col-sm-6 col-sm-offset-3">
                                     <div class="text-center">
-                                        <button type="submit" id="submit" name="submit" class="btn btn-send">Send </button>
+                                        <button type="submit" id="submit" name="submit" class="btn btn-send">Enviar </button>
                                     </div>
                                 </div>
                             </form>
@@ -775,7 +768,7 @@
                     <div class="col-md-6 col-md-offset-3">
                         <div class="copyright text-center">
                             <p>
-                                &copy; Copyright, 2015 <a href="#">Your Website Link.</a> Theme by <a href="http://themewagon.com/"  target="_blank">ThemeWagon</a>
+                                &copy; Copyright, 2018 <a href="#">Link do seu website</a> Desenvolvido por: <a href="#"  target="_blank">Alan Dantas</a>
                             </p>
                         </div>
                     </div>
